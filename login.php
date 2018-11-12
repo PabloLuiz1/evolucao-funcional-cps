937d014e3f3c9d312a0b863891c43763236cdedb
Pablo Luiz Ribeiro <pabloluiz.ribeiroo@gmail.com>
2018-11-11 22:45:26 -0200

    Segundo commit
    Criação dos diretórios "css" e "img". As páginas criadas pela Marina foram adicionadas ao projeto.

--- login.php
+++ login.php
@@ -1,9 +1,27 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
     <head>
-        <meta charset="utf-8">
-    <title>Página de login</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <title>Login</title>
     </head>
-<body>
-    A página de login irá conter o formulário de preenchimento de REGISTRO do docente e senha do mesmo. Para realizar a ação de login. 
-</body>
+    <body>
+        <header>
+            <img src="./img/header.jpg">
+        </header>
+    <div id="login">
+        <div style="padding:10px;color:white">Login</div>
+        <div id="formulario" style="height:210px;">
+        <form action="#">
+            <input type="text" name="registro" placeholder="Registro"><br>
+            <input type="text" name="senha" placeholder="Senha"><br>
+            <input id="button" type="submit" value="Cadastrar">
+            <input id="button" type="submit" value="Logar">
+        </form>
+        <a href="#">Esqueci minha senha</a><br>
+        </div>
+    </div>
+    <footer>
+        <img src="./img/footer.jpg">
+    </footer>
+    </body>
+</html>
