46364272dea0e2d355027b36dd9e26aa8cecda06
Pablo Luiz Ribeiro <pabloluiz.ribeiroo@gmail.com>
2018-11-12 23:27:08 -0200

    Quarto commit
    *Alterações no .sql do banco de dados
    *Início de criação do corpo das páginas mostradas ao usuário

--- db/dbevolucaofuncional.sql
+++ db/dbevolucaofuncional.sql
@@ -1,7 +1,6 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
---
 -- Host: 127.0.0.1:3306
 -- Generation Time: 12-Nov-2018 às 03:23
 -- Versão do servidor: 5.7.23
@@ -33,10 +32,15 @@ CREATE TABLE IF NOT EXISTS `tbarquivo` (
   `id_arquivo` int(11) NOT NULL,
   `id_solicitacao_arquivo` int(11) NOT NULL,
   `pontuacao_arquivo` int(11) NOT NULL,
-  `documento_arquivo` varchar(100) NOT NULL,
+  `pontuacao_final_arquivo` int (11) NOT NULL,
+  `tipo_arquivo` varchar(80) NOT NULL,
+  `nome_arquivo` varchar(100) NOT NULL,
+  `descricao_arquivo` varchar(300) NOT NULL,
+  `status_exclusao` tinyint (1) NOT NULL,
+  `comentario_arquivo` varchar (200) NOT NULL,
   PRIMARY KEY (`id_arquivo`),
-  KEY `fk_solicitacao` (`id_solicitacao_arquivo`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_solicitacao` FOREIGN KEY (`id_solicitacao_arquivo`) REFERENCES `tbsolicitacao` (`id_solicitacao`)
+) ENGINE=innodb DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -50,9 +54,12 @@ CREATE TABLE IF NOT EXISTS `tbsolicitacao` (
   `id_usuario_solicitacao` int(11) NOT NULL,
   `data_solicitacao` date NOT NULL,
   `pontuacao_total_solicitacao` int(11) NOT NULL,
+  `status_exclusao` tinyint (1) NOT NULL,
+  `status_solicitacao` varchar(50) NOT NULL,
+  `pontuacao_final_solicitacao` int(11) NOT NULL,
   PRIMARY KEY (`id_solicitacao`),
-  KEY `fk_usuario` (`id_usuario_solicitacao`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario_solicitacao`) REFERENCES `tbusuario` (`id_usuario`)
+) ENGINE=innodb DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -69,8 +76,9 @@ CREATE TABLE IF NOT EXISTS `tbusuario` (
   `login_usuario` varchar(20) NOT NULL,
   `senha_usuario` varchar(100) NOT NULL,
   `registro_usuario` varchar(100) NOT NULL,
+  `status_exclusao` tinyint (1) NOT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=innodb DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-    <title>Página inicial</title>
+        <title>Página inicial</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     </head>
 <body>
-    Na página inicial do sistema pode conter algumas informações sobre o processo de evolução funcional, bem como um link para uma espécie
-    de documento oficial no site do CPS... Notícias importantes e novidades sobre o processo de evolução funcional e etc.
+    <div id="container-fluid">
+        <header>
+            Na página inicial do sistema pode conter algumas informações sobre o processo de evolução funcional, bem como um link para uma espécie
+            de documento oficial no site do CPS... Notícias importantes e novidades sobre o processo de evolução funcional e etc.
+            <menu>
+
+            </menu>
+        </header>
+        Conteúdo
+        <footer>
+
+        </footer>
+    </div>
 </body>
